27b99ae1410677b602767af5fd21a051fef7c64c
josh0918 <joshuakc05@gmail.com>
2024-11-25 00:10:17 +0400

    Update MAIN.java

--- GUIProject/MAIN.java
+++ GUIProject/MAIN.java
@@ -1,41 +1,8 @@
-/**import java.awt.*;
-import javax.swing.*;
-import javax.swing.border.Border;
 
-public class Main {
-    private static void createAndShowGUI() {
-        JFrame jFrame = new JFrame("Hello World Swing Example");
-        jFrame.setLayout(new FlowLayout());
-        jFrame.setSize(500, 360);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+package javase;
 
-
-        System.out.println("help wrld");
-
-
-        JLabel label = new JLabel("Hello World Swing");
-        Border border = BorderFactory.createLineBorder(Color.BLACK);
-        label.setBorder(border);
-        label.setPreferredSize(new Dimension(150, 100));
-
-        label.setText("Hello World Swing");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
-
-        jFrame.add(label);
-        jFrame.setVisible(true);
-    }
-    
-  public static void main(String[] args) {
-       
-    createAndShowGUI();
-  }
-}**/
-
-// Don't mind the above code it is for later use to build the GUI
 import java.util.Scanner;
 
-can you see anything now?
 
 
 public class MAIN{
@@ -46,30 +13,7 @@ public class MAIN{
     }
 }
 
-// BEAMLAKU CODE
-/*class Item {
-    String itemName;
-    double itemPrice;
 
-    // Default constructor to read item details
-    Item() {
-        System.out.println("Enter the item name: ");
-        itemName = Main.input.nextLine(); // Use the shared input
-        System.out.println("Enter the item price: ");
-        itemPrice = Main.input.nextDouble();
-
-        Cart cart1 = new Cart();
-        cart1.addItem(itemName, itemPrice); // Add the item to the cart
-
-        TaxCalculator taxCalculator1 = new TaxCalculator();
-        taxCalculator1.calculatePriceWithTax(itemName, itemPrice, taxCalculator1.taxRate);
-    }
-
-    Item(String itemName, double itemPrice) {
-        this.itemName = itemName;
-        this.itemPrice = itemPrice;
-    }
-}*/
 abstract class Item {
     private String itemName;
     private double itemPrice;
@@ -108,28 +52,11 @@ class RegularItem extends Item {
 
     @Override
     public double calculatePriceWithTax(double taxRate) {
-        return getItemPrice() + (getItemPrice() * taxRate / 100);
-    }
-}
-
-// DiscountedItem Class
-class DiscountedItem extends Item {
-    private double discountRate; // Discount rate in percentage
-
-    public DiscountedItem(String name, double price, double discountRate) {
-        super(name, price);
-        this.discountRate = discountRate;
-    }
-
-    @Override
-    public double calculatePriceWithTax(double taxRate) {
-        double discountedPrice = getItemPrice() - (getItemPrice() * discountRate / 100);
-        return discountedPrice + (discountedPrice * taxRate / 100);
+        return TaxCalculator.calculatePriceWithTax(getItemPrice(), taxRate);
     }
 }
 
 
-// JOSHUA CODE
 class Cart  {
 
     RegularItem[] itemArr = new RegularItem[50];
@@ -175,11 +102,11 @@ class Cart  {
         for (int i = 0; i < arrLen; i++)
         {
 
-            price += itemArr[i].calculatePriceWithTax(taxRate);
+            price += itemArr[i].getItemPrice();
 
         }
 
-        return price;
+        return TaxCalculator.calculatePriceWithTax(price,taxRate);
         
     }
 
@@ -190,44 +117,16 @@ class Cart  {
 
     }
 
-    public boolean dupCheck(String name) // check for duplicate values such as names
-    {
-        if (arrLen != 0)
-        {
-            for (int i = 0; i < arrLen; i++) 
-            {
-                if (itemArr[i].getItemName().equals(name))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        } 
-        else
-        {
-            System.out.println("No items in cart");
-            return false;
-        }
-
-    }
-    public void displayItems()
-    {
-        for (int i = 0; i < arrLen; i++)
-            {
-                System.out.println(itemArr[i].getItemName() + " " + itemArr[i].getItemPrice());
-            }
-    }
+   
 }
 
-// ENKOPAZION CODE
+
 class TaxCalculator {
     Scanner TaxCalculatorInput = new Scanner(System.in);
     double taxRate, totalPrice;
 
     // Default constructor to read tax rate
     TaxCalculator() {
-        System.out.println("Enter the tax rate: ");
         taxRate = TaxCalculatorInput.nextDouble();
     }
 
@@ -235,9 +134,8 @@ class TaxCalculator {
         this.taxRate = taxRate;
     }
 
-    public static double calculatePriceWithTax(String itemName, double totalPrice, double taxRate) {
+    public static double calculatePriceWithTax(double totalPrice, double taxRate) {
         double priceWithTax = totalPrice + ((taxRate / 100) * totalPrice);
-        System.out.println("Total  price with tax is " + priceWithTax);
         return priceWithTax;
     }
 }
